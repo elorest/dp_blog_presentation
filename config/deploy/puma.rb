dcc356887d6bd1b6bd0a926b733aec7afdfc9799
Isaac Sloan <isaac@isaacsloan.com>
2016-03-07 23:30:22 -0700

    testing simpler cap script

--- config/deploy/puma.rb
+++ config/deploy/puma.rb
@@ -48,24 +48,24 @@ server urls.first, user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # fetch(:default_env).merge!(rails_env: :staging)
 # Change these
-set :puma_threads,    [4, 16]
-set :puma_workers,    2
-
-# Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/www/current/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+# set :puma_threads,    [4, 16]
+# set :puma_workers,    2
+#
+# # Don't change these unless you know what you're doing
+# set :pty,             true
+# set :use_sudo,        false
+# set :stage,           :production
+# set :deploy_via,      :remote_cache
+# set :deploy_to,       "/home/#{fetch(:user)}/www/current/#{fetch(:application)}"
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_access_log, "#{release_path}/log/puma.error.log"
+# set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+# set :puma_preload_app, true
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
