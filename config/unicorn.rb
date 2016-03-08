e64667d9d7b29b85976a0e33cc63bc37b6980657
Cameron Kidman <cameron.kidman@mx.com>
2016-03-08 08:49:35 -0700

    This should work

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 # set path to application
 app_dir = "/home/deploy/www/unicorn"
 shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+working_directory "#{app_dir}/current"
 
 
 # Set unicorn options
